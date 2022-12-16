f2abb7103cd099d9a056a0f221a88ff0e17cfc89
jerry <jerry19971228@gmail.com>
2022-12-16 15:59:03 +0800

    使用物件DB來取得資料表的資料->all()

---
+++ base.php
@@ -0,0 +1,76 @@
+<?php
+
+$Student=new DB('students');
+
+// var_dump($Student);
+
+$stus=$Student->all();
+foreach($stus as $stu){
+    echo $stu['parents'];
+    echo "<br>";
+}
+
+class DB{
+    protected $table;
+    protected $dsn="mysql:host=localhost;charset=utf8;dbname=school";
+    protected $pdo;
+
+    public function __construct($table)
+    {
+        $this->pdo=new PDO($this->dsn,'root','');
+        $this->table=$table;
+    }
+
+    public function all(...$arg){
+        // 原本函式all()的內容
+        // global $pdo;
+    // $sql = "SELECT * FROM $table ";
+
+    // if (isset($args[0])) {
+        // if (is_array($args[0])) {
+            // foreach ($args[0] as $key => $value) {
+                // $tmp[] = "`$key`='$value'";
+            // }
+            // $sql = $sql . " WHERE " . join(" && ", $tmp);
+        // } else {
+            // $sql = $sql . $args[0];
+        // }
+    // }
+    // if (isset($args[1])) {
+        // $sql = $sql . $args[1];
+    // }
+    // echo $sql;
+    // return $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    // }
+
+    $sql = "select * from $this->table";
+
+    if (isset($args[0])) {
+        if (is_array($args[0])) {
+            // 是陣列 ['acc' => 'qwer1', 'pw' => 'qwer1'];
+            // 是陣列 ['product' => 'pc', 'price' => '10000'];
+
+            foreach ($args[0] as $key => $value) {
+                $tmp[] = "`$key`='$value'";
+            }
+
+            $sql = $sql . " WHERE " . join(" && ", $tmp);
+        } else {
+            // 是字串
+            $sql = $sql . $args[0];
+        }
+    }
+
+    if (isset($args[1])) {
+        $sql = $sql . $args[1];
+    }
+
+    echo $sql;
+    return $this->pdo
+                ->query($sql)
+                ->fetchAll(PDO::FETCH_ASSOC);
+}
+}
+
+
+?>
