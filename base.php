5345d0d21532d413ba51d48e9945d131a660a900
jerry <jerry19971228@gmail.com>
2022-12-19 10:48:05 +0800

    增加一個save()函式，依id的有無來進行新增或是更新資料

--- base.php
+++ base.php
@@ -1,23 +1,30 @@
-<?php
+<?php 
 
 $Student=new DB('students');
 
-// var_dump($Student);
-// $john=$Student->find(30);
-// echo $john['name'];
+//var_dump($Student);
+/* $john=$Student->find(30);
+echo $john['name']; */
 
-// $Student->del(10);
-// $Student->del(['dept'=>1]);
+//$Student->del(10);
+/* $Student->del(['dept'=>1]);
 
+$stus=$Student->all(['dept'=>3]);
+foreach($stus as $stu){
+    echo $stu['parents'] . "=>".$stu['dept'];
+    echo "<br>";
+} */
 
-// $stus=$Student->all(['dept'=>3]);
-// foreach($stus as $stu){
-//     echo $stu['parents']."=>".$stu['dept'];
-//     echo "<br>";
-// }
-
-// 新增資料
+//新增資料
 $Student->save(['name'=>'張大同','dept'=>2,'uni_id'=>"H22211223"]);
+
+echo "<hr>";
+//更新資料
+$Student->save(['name'=>'張大同','dept'=>2,'uni_id'=>"H22211223",'id'=>3]);
+$stu=$Student->find(15);
+dd($stu);
+$stu['name']="陳秋桂";
+$Student->save($stu);
 class DB{
     protected $table;
     protected $dsn="mysql:host=localhost;charset=utf8;dbname=school";
@@ -29,47 +36,55 @@ class DB{
         $this->table=$table;
     }
 
+
     public function all(...$args){
-        // 原本函式all()的內容
-        // global $pdo;
-    // $sql = "SELECT * FROM $table ";
+/*
+  原本函式all()的內容
+global $pdo;
+    $sql="select * from $table ";
 
-    // if (isset($args[0])) {
-        // if (is_array($args[0])) {
-            // foreach ($args[0] as $key => $value) {
-                // $tmp[] = "`$key`='$value'";
-            // }
-            // $sql = $sql . " WHERE " . join(" && ", $tmp);
-        // } else {
-            // $sql = $sql . $args[0];
-        // }
-    // }
-    // if (isset($args[1])) {
-        // $sql = $sql . $args[1];
-    // }
-    // echo $sql;
-    // return $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
-    // }
+    if(isset($args[0])){
+        if(is_array($args[0])){
+            //是陣列 ['acc'=>'mack','pw'=>'1234'];
+            //是陣列 ['product'=>'PC','price'=>'10000'];
 
-    $sql = "select * from $this->table";
-
-    if (isset($args[0])) {
-        if (is_array($args[0])) {
-            // 是陣列 ['acc' => 'qwer1', 'pw' => 'qwer1'];
-            // 是陣列 ['product' => 'pc', 'price' => '10000'];
-
-            foreach ($args[0] as $key => $value) {
-                $tmp[] = "`$key`='$value'";
+            foreach($args[0] as $key => $value){
+                $tmp[]="`$key`='$value'";
             }
 
-            $sql = $sql . " WHERE " . join(" && ", $tmp);
-        } else {
-            // 是字串
-            $sql = $sql . $args[0];
+            $sql=$sql ." WHERE ". join(" && " ,$tmp);
+        }else{
+            //是字串
+            $sql=$sql . $args[0];
         }
     }
 
-    if (isset($args[1])) {
+    if(isset($args[1])){
+        $sql = $sql . $args[1];
+    }
+
+    //echo $sql;
+    return $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC); */
+
+    $sql="select * from $this->table ";
+
+    if(isset($args[0])){
+        if(is_array($args[0])){
+            //是陣列 ['acc'=>'mack','pw'=>'1234'];
+            //是陣列 ['product'=>'PC','price'=>'10000'];
+
+            foreach($args[0] as $key => $value){
+                $tmp[]="`$key`='$value'";
+            }
+
+            $sql=$sql ." WHERE ". join(" && " ,$tmp);
+        }else{
+            //是字串
+            $sql=$sql . $args[0];
+        }
+    }
+
+    if(isset($args[1])){
         $sql = $sql . $args[1];
     }
 
@@ -77,60 +92,85 @@ class DB{
     return $this->pdo
                 ->query($sql)
                 ->fetchAll(PDO::FETCH_ASSOC);
-}
+
+    }
+
 
     function find($id){
-        $sql = "SELECT * FROM `$this->table` ";
+        $sql="select * from `$this->table` ";
 
-        if (is_array($id)) {
-            foreach ($id as $key => $value) {
-                $tmp[] = "`$key`='$value'";
+        if(is_array($id)){
+            foreach($id as $key => $value){
+                $tmp[]="`$key`='$value'";
             }
     
-            $sql = $sql . " WHERE " . join(" && ", $tmp);
-        } else {
-            $sql = $sql . "WHERE `id`='$id'";
-        }
+            $sql = $sql . " where " . join(" && ",$tmp);
     
+        }else{
+    
+            $sql=$sql . " where `id`='$id'";
+        }
+        //echo $sql;
         return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
-}
+    }
+
 
     function del($id){
-        $sql = "DELETE FROM `$this->table` ";
+        $sql="delete from `$this->table` ";
 
-     if (is_array($id)) {
-            foreach ($id as $key => $value) {
-                $tmp[] = "`$key`='$value'";
+        if(is_array($id)){
+            foreach($id as $key => $value){
+                $tmp[]="`$key`='$value'";
             }
-
-            $sql = $sql . " WHERE " . join(" && ", $tmp);
-        } else {
-            $sql = $sql . "WHERE `id`='$id'";
+    
+            $sql = $sql . " where " . join(" && ",$tmp);
+    
+        }else{
+    
+            $sql=$sql . " where `id`='$id'";
         }
+
         echo $sql;
         return $this->pdo->exec($sql);
-}
 
-        function save($array){
+    }
+
+    function save($array){
         if(isset($array['id'])){
-                //更新update
-            }else{
-             //新增insert
-                $cols=array_keys($array);
-    
-                $sql="insert into `$this->table` (`" . join("`,`",$cols) . "`) 
-                                        values('" . join("','",$array) . "')";
-
-             echo $sql;
-                return $this->pdo->exec($sql);
+            //更新update
+            foreach($array as $key => $value){
+              /*if($key!='id'){
+                    $tmp[]="`$key`='$value'";
+                } */
+                if($key!='id'){
+                    $tmp[]="`$key`='$value'";
+                }
             }
 
-        }
-    
-        function dd($array){
-            echo "<pre>";
-            print_r($array);
-            echo "</pre>";
+            $sql ="update $this->table set ";
+            $sql .=join(",",$tmp);
+            $sql .=" where `id`='{$array['id']}'";
+
+        }else{
+            //新增insert
+            $cols=array_keys($array);
+        
+            $sql="insert into $this->table (`" . join("`,`",$cols) . "`) 
+                                     values('" . join("','",$array) . "')";
+
         }
 
+            // echo $sql;
+            return $this->pdo->exec($sql);
+
+    }
+
 }
+
+function dd($array){
+    echo "<pre>";
+    print_r($array);
+    echo "</pre>";
+}
+
+?>
